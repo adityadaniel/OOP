aaf5e847f45e39c91331b163b538ed71cad39dc1
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2019-05-15 08:14:26 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Daniel Aditya Istyana on 15/05/19.
+//  Copyright © 2019 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
